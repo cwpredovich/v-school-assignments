3f2297a8ad84dcc4a7be1908dcc356e30f5640c2
Christopher <cwpredovich@yahoo.com>
2018-09-06 16:37:23 -0600

    React Name Badge assignment. This included the use of props and stateful components

--- exercises/react-name-badges/src/App.js
+++ exercises/react-name-badges/src/App.js
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
-import './App.css';
-import './Form.js';
+import Form from './Form';
+import Display from './Display';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fName: "",
+      lName: "",
+      email: "",
+      birthPlace: "",
+      phone: "",
+      favFood: "",
+      about: "",
+      nameCards: []
+    }
+  }
+
+  // write 2 functions. 1st is for handleChange, and 2nd is for handleSubmit.
+  // I MIGHT need to do handleSubmit here, as well
+  // This website has helpful info for this project:
+  // https://reactjs.org/docs/forms.html
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    
+  }
+
+    // Handle Submit Function:
+    // I want to be able to click submit,
+    // have the data all saved to an object
+    // add that object to the array of name cards
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newContact = {
+      fName: this.state.fName,
+      lName: this.state.lName,
+      email: this.state.email,
+      birthPlace: this.state.birthPlace,
+      phone: this.state.phone,
+      favFood: this.state.favFood
+    }
+    this.setState(prevState => ({
+      nameCards: [...prevState.nameCards, newContact],
+      fName: '',
+      lName: '',
+      email: '',
+      birthPlace: '',
+      phone: '',
+      favFood: ''
+    }))
+    
+  }
+
+
   render() {
     return (
       <div>
-        <Form />
+        <Form {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
         <Display />
+        {console.log(this.state)}
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
--- exercises/react-name-badges/src/Form.js
+++ exercises/react-name-badges/src/Form.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-function Form() {
+const Form = (props) => {
     return(
         <div>
-            <form>
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
-                <input />
+            <form onSubmit={props.handleSubmit}>
+                <input type="text" name="fName" value={props.fName} placeholder="First Name" onChange={props.handleChange}/>
+                <input name="lName" value={props.lName} placeholder="Last Name" onChange={props.handleChange}/>
+                <input name="email" value={props.email} placeholder="Email" onChange={props.handleChange}/>
+                <input name="birthPlace" value={props.birthPlace} placeholder="Place of Birth" onChange={props.handleChange}/>
+                <input name="phone" value={props.phone} placeholder="Phone" onChange={props.handleChange}/>
+                <input name="favFood" value={props.favFood} placeholder="Favorite Food" onChange={props.handleChange}/>
+
+                 {/* name and value match the part of state they are meant to update */}
+                 {/* need an onChange attribute for the input as well */}
+                <textarea name="about" value={props.about} placeholder="Tell us about yourself" onChange={props.handleChange}/>
+                <button>Submit</button>
             </form>
         </div>
     )
